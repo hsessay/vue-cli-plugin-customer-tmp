bba740f6f5db4d707354d4687c7ebdf401326cdd
zhouxinyu <zhouxinyu@zhouxinyudeMacBook-Pro.local>
2019-03-26 17:37:12 +0800

    test

--- generator/templates/base/src/App.vue
+++ generator/templates/base/src/App.vue
@@ -1,27 +1,16 @@
 <template>
   <div id="app"> 
+    <router-view/> 
   </div>
 </template>
+
 <script> 
 
-export default { 
-  methods: {
-     
-  }, 
-};
+export default {
+  name: 'App'
+}
 </script>
 
 <style>
-@import "./src/assets/css/common.css";
-.xd-maxheight {
-  height: 60px;
-}
-.xd-maxwidth {
-  max-width: 640px;
-  margin: auto;
-  left: 50%;
-  -webkit-transform: translate(-50%, 0);
-  -ms-transform: translate(-50%, 0);
-  transform: translate(-50%, 0);
-}
+
 </style>
