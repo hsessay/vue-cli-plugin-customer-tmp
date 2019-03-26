00a3e78285f0f9ec3662d5211eecc6d537a3f7fc
zhouxinyu <zhouxinyu@zhouxinyudeMacBook-Pro.local>
2019-03-26 17:18:45 +0800

    test

--- generator/index.js
+++ generator/index.js
@@ -1,62 +1,54 @@
-const fs = require ('fs');
+function renderFiles (api, opts) {
+
+  const fs = require('fs')
+
+  // 通过preset的形式配置opts.router，这里则不需要
+  const routerPath = api.resolve('./src/router.js')
+  opts.router = opts.router || fs.existsSync(routerPath)
+
+  const filesToDelete = [
+    'src/assets/logo.png',
+    'src/views/About.vue',
+    'src/views/Home.vue',
+  ]
+
+  console.log('\n[custom-tpl plugin tips]\n \t GeneratorAPI options:', opts)
+
+  if (opts.replaceTemplates) {
+
+    // https://github.com/vuejs/vue-cli/issues/2470
+    api.render(files => {
+      Object.keys(files)
+        .filter(name => filesToDelete.indexOf(name) > -1)
+        .forEach(name => delete files[name])
+    })
+
+    api.render('./templates/base')
+
+    if (opts.router) {
+      // 替换掉路由文件
+      api.render('./templates/sp')
+    }else{
+    }
+
+  }
+}
+
+function addDependencies (api) {
+  api.extendPackage({
+    dependencies: {
+      "axios": "^0.18.0",
+    },
+    devDependencies: {
+      // "less": "^3.0.4",
+      // "less-loader": "^4.1.0",
+    }
+  })
+}
 
 module.exports = (api, opts, rootOpts) => {
-  // 添加 npm 命令
-  api.extendPackage ({
-    scripts: {
-      dev: 'vue-cli-service serve --copy',
-      build: 'vue-cli-service build',
-      review: 'serve -s dist',
-      lint: 'vue-cli-service lint',
-      serve: 'vue-cli-service serve',
-    },
-  });
 
-  // 开发依赖包
-  api.extendPackage ({
-    devDependencies: {
-      serve: '^10.0.1',
-      'style-resources-loader': '1.2.1',
-      "vue-template-compiler": "^2.5.21",
-      less: '^2.7.2',
-      'less-loader': '^3.0.0',
-      "vue-template-compiler": '^2.5.21', 
-    },
-  });
+  addDependencies(api)
 
-  api.extendPackage ({
-    dependencies: {
-      flyio: '^0.6.14', 
-      lodash: '^4.17.11',
-      'normalize.css': '^8.0.0',
-      nprogress: '^0.2.0',
-      countup: '^1.8.2', 
-      vant: '^1.6.9',
-      "weixin-js-sdk": "^1.4.0-test",
-      "vue-qr": "^1.5.2",
-      vconsole: "^3.3.0",
-    },
-  }); 
-
-  // 扩展 .eslintrc 配置
-  api.extendPackage ({
-    eslintConfig: {
-      rules: {
-        'vue/no-parsing-error': [2, {'x-invalid-end-tag': false}],
-      },
-    },
-  });
-
-  // 删除多余的模板
-  api.render (files => {
-    Object.keys (files)
-      .filter (path => path.startsWith ('src/') || path.startsWith ('public/') || path.startsWith('tests/'))
-      .forEach (path => delete files[path]);
-  });
-
-  api.render('./templates');
- 
-  api.onCreateComplete(() => {
-    process.env.VUE_CLI_SKIP_WRITE = true
-  })
-};
+  renderFiles(api, opts)
+}
