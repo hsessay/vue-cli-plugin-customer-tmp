eec621a9aef6742fc20b4cac3816d994b9a71f9c
zhouxinyu <zhouxinyu@zhouxinyudeMacBook-Pro.local>
2019-03-27 13:43:38 +0800

    m

--- generator/templates/base/src/views/login/LoginWarranty.vue
+++ generator/templates/base/src/views/login/LoginWarranty.vue
@@ -1,7 +1,7 @@
 <!-- 登录页面 -->
 <template>
   <div>
-    <img src="../../assets/images/xd-home-banner.png" width="100%">
+    <!-- <img src="../../assets/images/xd-home-banner.png" width="100%">
     <register-portrait ref="getInput" v-on:getVerifyCode="getVerifyCodeAction"/>
     <div class="xd_btn_t">
       <van-button type="primary" size="normal" @click="login">登录</van-button>
@@ -9,22 +9,20 @@
         没有账号，
         <a href="javascript:;" @click="toRegisterPage">去注册</a>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 // 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 // 例如：import 《组件名称》 from '《组件路径》';
-import RegisterPortrait from "./components/RegisterPortrait";
-import { Button } from "vant";
-import { getVerifyCode, msgLoginService } from "../../api/service.js";
+// import RegisterPortrait from "./components/RegisterPortrait"; 
+// import { getVerifyCode, msgLoginService } from "../../api/service.js";
 
 export default {
   // import引入的组件需要注入到对象中才能使用
   components: {
-    RegisterPortrait,
-    [Button.name]: Button
+    // RegisterPortrait, 
   },
   data() {
     // 这里存放数据
@@ -40,51 +38,51 @@ export default {
   watch: {},
   // 方法集合
   methods: {
-    toRegisterPage() {
-      this.$router.push({ path: "register" }); // 这里可以配置参数，具体参考官网
-    },
-    login() {
-      var phoneNum = this.$refs.getInput.phoneNumber;
-      var verifyCode = this.$refs.getInput.verifyCode;
-      var errMsg = "";
+    // toRegisterPage() {
+    //   this.$router.push({ path: "register" }); // 这里可以配置参数，具体参考官网
+    // },
+    // login() {
+    //   var phoneNum = this.$refs.getInput.phoneNumber;
+    //   var verifyCode = this.$refs.getInput.verifyCode;
+    //   var errMsg = "";
 
-      if (verifyCode.length === 0) {
-        errMsg = "请输入验证码";
-      }
+    //   if (verifyCode.length === 0) {
+    //     errMsg = "请输入验证码";
+    //   }
 
-      if (errMsg.length > 0) {
-        this.$dialog.alert({
-          title: "信息错误",
-          message: errMsg,
-          confirmButtonText: "知道了"
-        });
-        return;
-      }
-      var _this = this;
-      msgLoginService(phoneNum, this.codeId, verifyCode)
-        .then(function(res) {
-          _this.$store.commit("updateUserInfo", res);
-          _this.jumpToNextPage();
-        }) 
-    },
+    //   if (errMsg.length > 0) {
+    //     this.$dialog.alert({
+    //       title: "信息错误",
+    //       message: errMsg,
+    //       confirmButtonText: "知道了"
+    //     });
+    //     return;
+    //   }
+    //   var _this = this;
+    //   msgLoginService(phoneNum, this.codeId, verifyCode)
+    //     .then(function(res) {
+    //       _this.$store.commit("updateUserInfo", res);
+    //       _this.jumpToNextPage();
+    //     }) 
+    // },
 
-    jumpToNextPage() {
-      if (this.$route.query.type === "getCustomer") {
-        this.$router.replace({ path: "estimatepre" }); 
-      } else {
-        this.$router.replace({ path: "minepersonal" });
-      }
-      console.log(this.$router)
-    },
+    // jumpToNextPage() {
+    //   if (this.$route.query.type === "getCustomer") {
+    //     this.$router.replace({ path: "estimatepre" }); 
+    //   } else {
+    //     this.$router.replace({ path: "minepersonal" });
+    //   }
+    //   console.log(this.$router)
+    // },
 
-    getVerifyCodeAction() {
-      this.phoneNum = this.$refs.getInput.phoneNumber;
-      var _this = this;
-      getVerifyCode(this.phoneNum)
-        .then(function(resp) {
-          _this.codeId = resp.sessionId;
-        }) 
-    }
+    // getVerifyCodeAction() {
+    //   this.phoneNum = this.$refs.getInput.phoneNumber;
+    //   var _this = this;
+    //   getVerifyCode(this.phoneNum)
+    //     .then(function(resp) {
+    //       _this.codeId = resp.sessionId;
+    //     }) 
+    // }
   }
 };
 </script>
