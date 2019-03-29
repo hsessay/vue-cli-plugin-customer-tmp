b82dba74d52494d1499b79bc98955e8cd5b01017
zhouxinyu <zhouxinyu@zhouxinyudeMacBook-Pro.local>
2019-03-29 16:12:21 +0800

    modif

--- docs/.vuepress/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    title: 'vue-cli3-preset-seed 文档',
-    themeConfig: {
-        sidebar: 'auto',
-        repo: 'https://github.com/yuezhilunhui2009/vue-cli3-preset-seed',
-    }
-  }
