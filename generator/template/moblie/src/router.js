3d42dcc2c6bdfbe5d011215807092bcecebafa84
zhouxinyu <zhouxinyu@hanhua.com>
2019-10-09 09:51:39 +0800

    update new information

--- generator/template/moblie/src/router.js
+++ generator/template/moblie/src/router.js
@@ -13,19 +13,25 @@ const router = new Router({
   routes: [
     {
       path: '/',
-      redirect: { name: 'login' }
+      redirect: { path: '/empty' }
+      // meta: { noLoginAuth: true }
     },
     {
-      path: '/login', // 登录页面
-      name: 'login',
-      component: () => import('./views/login/LoginPage.vue'),
-      // meta: { noLoginAuth: true }
-    }
+      path: '/empty',
+      name: 'empty',
+      component: () => import('./components/EmptyView.vue')
+    },
   ]
 })
 
 // 全局前置路由权鉴
 router.beforeEach((to, from, next) => {
+  if (to.name === 'empty') {
+    let domain = process.env.VUE_APP_DOMAIN
+    window.location.href = domain
+    return
+  }
+
   if (!to.meta.noLoginAuth && !Vue.prototype.$userInfo) {
     let { openid, token } = to.query
     let loginPath = `${config.api}${config.basePath}/login?redirect=${config.orginUrl}${config.originBasePath}${to.fullPath}`
