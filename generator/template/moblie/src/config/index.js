cd226a502c51dcf48ab3fb60aae1da45ce338d4e
zxy <510773653@qq.com>
2021-04-23 17:34:00 +0800

    Update index.js

--- generator/template/moblie/src/config/index.js
+++ generator/template/moblie/src/config/index.js
@@ -5,41 +5,40 @@ const modeUrlObj = {
     orginUrl: window.location.origin,
     domain: location.protocol + '//t.hanhua.com',
     basePath: process.env.VUE_APP_BASEURL,
-    fmBankNo: '8808001000322821', // 富民商户号
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL' // 需要申请还是用开发ak?
+    downloadUrl: '',
+    uploadUrl: ''
   },
   // 预生产环境
   preproduction: {
     orginUrl: window.location.origin,
     domain: location.protocol + '//www.hanhuatong.com.cn',
     basePath: process.env.VUE_APP_BASEURL,
-    fmBankNo: '8808001000322821',
-    fmMerchantNo: '',
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL'
+    downloadUrl: '',
+    uploadUrl: ''
   },
   // 开发环境
   development: {
     orginUrl: window.location.origin,
     domain: location.protocol + '//htsit.hanhua.com',
     basePath: '',
-    fmBankNo: '8808001000322821',
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL'
+    downloadUrl: '',
+    uploadUrl: ''
   },
   // 测试环境
   sit: {
     orginUrl: window.location.origin,
     domain: location.protocol + '//htsit.hanhua.com',
     basePath: process.env.VUE_APP_BASEURL,
-    fmBankNo: '8808001000322821',
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL'
+    downloadUrl: '',
+    uploadUrl: ''
   },
   // 测试环境
   uat: {
     orginUrl: window.location.origin,
     domain: location.protocol + '//htuat.hanhua.com',
     basePath: process.env.VUE_APP_BASEURL,
-    fmBankNo: '8808001000322821',
-    baiduAk: 'qaHv1ASHKazGKMPuMp9feXbm4WpSRL'
+    downloadUrl: '',
+    uploadUrl: ''
   }
 }
 const configObj = {
